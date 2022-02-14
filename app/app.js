6ab13b5766e25805f4d23b3b63346a7b85ab184b
= <matdombrock@gmail.com>
2022-02-14 11:40:39 +0000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const config = require('../config');
 const usersList = require('../users.json');
 
+const fs = require('fs');
+
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-//const bodyParser = require('body-parser');
 const routes = require('./routes');
 const auth = require("./middleware/auth");
 const logger = require('./middleware/logger');
@@ -50,8 +51,28 @@ app.post('/upload', auth, routes.upload);
 app.post('/login', routes.login);
 
 async function start(){
+  // create dirs
+  if (!fs.existsSync(config.local_incoming_dir)){
+    console.log('Created incoming directory at: '+config.local_incoming_dir);
+    fs.mkdirSync(config.local_incoming_dir,{ recursive: true });
+  }else{
+    console.log('Found incoming directory at: '+config.local_incoming_dir);
+  }
+  if (!fs.existsSync(config.local_public_dir)){
+    console.log('Created public directory at: '+config.local_public_dir);
+    fs.mkdirSync(config.local_public_dir,{ recursive: true });
+  }else{
+    console.log('Found public directory at: '+config.local_public_dir);
+  }
+  if (!fs.existsSync(config.local_private_dir)){
+    console.log('Created private directory at: '+config.local_private_dir);
+    fs.mkdirSync(config.local_private_dir,{ recursive: true });
+  }else{
+    console.log('Found private directory at: '+config.local_private_dir);
+  }
   // createUsers
   await createUser.createList(usersList);
+  // start server
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
   })
